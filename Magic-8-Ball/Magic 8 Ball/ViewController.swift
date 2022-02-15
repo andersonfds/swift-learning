e2cde7d93081d1c4ecadec1977cd6a234bc78c4b
Anderson Freitas <anderson.santos@willbank.com.br>
2022-02-15 02:38:18 -0300

    feat: implemented the magic ball of answers

--- Magic-8-Ball/Magic 8 Ball/ViewController.swift
+++ Magic-8-Ball/Magic 8 Ball/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var magicBall: UIImageView!
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-
+    let ballArray : Array<UIImage> = [
+        UIImage(imageLiteralResourceName: "ball1"),
+        UIImage(imageLiteralResourceName: "ball2"),
+        UIImage(imageLiteralResourceName: "ball3"),
+        UIImage(imageLiteralResourceName: "ball4"),
+        UIImage(imageLiteralResourceName: "ball5"),
+    ]
+    
+    @IBAction func onAsk(_ sender: UIButton) {
+        magicBall.image = ballArray.randomElement()
+    }
 }
 
